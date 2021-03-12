906dddf0fa7aa526027faaff000b46043bf54035
Adrian Hartanto <42912948+adrianfile@users.noreply.github.com>
2021-03-12 14:44:43 +0700

    About progress

--- src/component/assets/About.js
+++ src/component/assets/About.js
@@ -2,9 +2,15 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <p> About Page </p>
-        </div>
+        <section>
+            <div class='flex sm:w-2/4 bg-red-100 sm:rounded-lg shadow mx-auto'>
+                <div class="mt-2 ">
+                   <p class='aboutme text-2xl font-bold uppercase sm:text-center'>about me</p>
+                   <br></br>
+                   <p class='aboutme font-thin sm:text-justify'>Web Developer, Founder, and CTO of Scratch Idea. I like confusing things like math and also love to help out with programming. I spent time sharing my self-taught experience learning Website Programming.<br></br><br></br>Currently, I'm a 3 year student at Areta Informatics College. I majored in informatics engineering. I took to my focus on programming development.</p>
+                </div>
+            </div>
+        </section>
     );
 };
 
